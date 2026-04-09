4898e8d1a5a30a435557172bc9a077be163f0709
Stephen Akoto <akotosteven@gmail.com>
2026-04-08 20:03:16 -0500

    Add Docker Compose setup and Airflow configuration for pipeline orchestration

---
+++ dags/taxi_pipeline_dag.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import sys
+
+sys.path.append('/opt/airflow')
+
+from src.ingestion.ingest_data import fetch_data, upload_to_s3
+
+def etl_pipeline():
+    df = fetch_data()
+    upload_to_s3(df)
+
+with DAG(
+    dag_id="taxi_data_pipeline",
+    start_date=datetime(2026, 4, 1),
+    schedule="@daily",
+    catchup=False,
+    tags=["data-engineering", "aws"]
+) as dag:
+
+    run_etl = PythonOperator(
+        task_id="run_etl_pipeline",
+        python_callable=etl_pipeline
+    )
+    
--- src/ingestion/ingest_data.py
+++ src/ingestion/ingest_data.py
@@ -1,15 +1,39 @@
-import requests
+from io import StringIO
+import boto3
 import pandas as pd
 
-URL = "https://raw.githubusercontent.com/DataTalksClub/nyc-tlc-data/main/data/yellow_tripdata_2021-01.csv"
+
+BUCKET_NAME = "stv-taxi-data-pipeline"
+FILE_KEY = "raw/taxi-data/yellow_tripdata_2021-01.csv"
+
+URL = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet"
 
 
 def fetch_data():
-    df = pd.read_csv(URL)
+    data = pd.read_parquet(URL)
     print("Data fetched successfully")
-    return df
+    return data
+
+
+def upload_to_s3(data):
+    csv_buffer = StringIO()
+    data.to_csv(csv_buffer, index=False)
+
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id="AKIARIOKTUWT72LZPAEO",
+        aws_secret_access_key="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",
+    )
+
+    s3.put_object(
+        Bucket=BUCKET_NAME,
+        Key=FILE_KEY,
+        Body=csv_buffer.getvalue()
+    )
+
+    print("Upload to S3 successful!")
 
 
 if __name__ == "__main__":
     df = fetch_data()
-    print(df.head())
+    upload_to_s3(df)
